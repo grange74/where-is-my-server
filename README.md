7bef3156d9833426141c7cd11142ef6816829638
nick.grange <nicolas.grange@retrievercommunications.com>
2015-02-11 23:55:58 +1100

    Added initial json encoding for user.

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 A REST API service in GO to connect up Users and their designated Server.
 
-A simple project to allow me to learn GO. 
+A simple project to allow me to learn GO. While there are some nice frameworks and libraries out there, my aim is to use the GO built-in libraries wherever possible.
+
+## Tools used for this project
+
+- Using [Sublime Text 2](http://www.sublimetext.com/2) with [GoSublime](https://github.com/DisposaBoy/GoSublime) package for editing GO files.
+- Also using a cool utility called [Gin](https://github.com/codegangsta/gin) to reloaded my Web Server code as soon as I modified it. Run using `gin -a "8080" run`.
--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func handler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "Where is my Server %s?", req.URL.Path[1:])
+const (
+	CONTENT_TYPE      = "Content-Type"
+	JSON_CONTENT_TYPE = "application/json; charset=UTF-8"
+)
+
+type User struct {
+	Name  string
+	Email string
+}
+
+func handleRoot(writer http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(writer, "Where is my Server %s?", req.URL.Path[1:])
+}
+
+func handleUser(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set(CONTENT_TYPE, JSON_CONTENT_TYPE)
+	writer.WriteHeader(http.StatusOK)
+
+	user := User{"Test User 1", "testuser1@gmail.com"}
+
+	if err := json.NewEncoder(writer).Encode(user); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/user", handleUser)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
